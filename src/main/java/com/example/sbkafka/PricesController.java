1af85bbd4c51e653ec53748c1e013454fee7f753
Администратор <@0300NBA0345C674>
2021-08-02 15:36:58 +0300

    test

--- src/main/java/com/example/sbkafka/PricesController.java
+++ src/main/java/com/example/sbkafka/PricesController.java
@@ -1,5 +1,7 @@
 package com.example.sbkafka;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,11 +18,11 @@ public class PricesController {
 	@GetMapping("/priceChanges")
 	public String showPrices(Model model) {
 		
-		priceService.findAll().forEach(it->System.out.println(it));
+		List<Price> prices=priceService.findAll();
 		
-		model.addAttribute("DGU3_CONTRACTOR_TIMEHOURES_WORKALARM", MyConstants.DGU3_CONTRACTOR_TIMEHOURES_WORKALARM);
+		model.addAttribute("prices",prices);
 		
-	    return "prices";
+	    return "test";
 	}
 	
 	@PostMapping("/priceChanges")
