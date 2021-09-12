fb6899bef126a588ed50d0c659f5a4e4ab63eab1
Администратор <@0300NBA0345C674>
2021-09-12 23:43:41 +0300

    exel

--- src/main/java/com/example/sbkafka/BdController.java
+++ src/main/java/com/example/sbkafka/BdController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class BdController {
 	
 	int str;int numstr;
-	ArrayList<Order> orderlistFile=new ArrayList<Order>();
+	
 	
 	@Autowired
 	private OrderFormService orderformservice;
@@ -25,7 +25,8 @@ public class BdController {
 	
 	@GetMapping("/bdShow") //получение инфо из бд заказов
 	public String bdShow(@RequestParam(value="str",defaultValue = "0") int str,@RequestParam(value="numstr",defaultValue = "20") int numstr,Model model){
-		orderlistFile.clear();
+		//orderlistFile.clear();
+		ArrayList<Order> orderlistFile=new ArrayList<Order>();
 		this.str=str;
 		this.numstr=numstr;
 		Page<OrderForm> orderlist=orderformservice.findAllWithPageAsc(this.str,this.numstr);
