af6efbd7ae41122cf45a8961763ea7600aeadf38
Администратор <@0300NBA0345C674>
2021-09-19 12:45:14 +0300

    copy to csv final

--- src/main/java/com/example/sbkafka/ExelController.java
+++ src/main/java/com/example/sbkafka/ExelController.java
@@ -2,7 +2,6 @@ package com.example.sbkafka;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -10,7 +9,6 @@ import java.sql.SQLException;
 
 import org.postgresql.copy.CopyManager;
 import org.postgresql.core.BaseConnection;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -24,25 +22,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ExelController {
 	
-	@Autowired OrderFormService orderformservice;
-	
-	@GetMapping("/downLoadExel")
-		ResponseEntity<Resource> getFile(){
-			String filename="dinamic_orders.xlsx";
-			InputStreamResource file=new InputStreamResource(orderformservice.load());
-			
-			return ResponseEntity.ok()
-					.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + filename)
-			        .contentType(MediaType.parseMediaType("application/vnd.ms-excel"))
-			        .body(file);
-		}
-		
-	@GetMapping("/saveToCsv")
-	public String saveToCsv() {
-		orderformservice.saveToCsv();
-		return "redirect:/bdShow";
-	}
-	
 	@GetMapping("/copyToCsv")
 	ResponseEntity<Resource> getFileCsv() throws SQLException, IOException{
 		String filename="dinamic_orders.xlsx";
@@ -54,36 +33,23 @@ public class ExelController {
 		        .body(file);
 	}
 	
-	public static InputStreamResource copyToFile() //(Connection connection, String filePath, String tableOrQuery)
-	          throws SQLException, IOException {  
+	public static InputStreamResource copyToFile() throws SQLException, IOException {  
 	      
-		  String urls = new String("jdbc:postgresql://ec2-54-155-254-112.eu-west-1.compute.amazonaws.com:5432/d747lpck3drmh3?sslmode=require"); //URL of the database
-	      String username = new String("ecwnrxyfpgybjl"); //User name
-	      String password = new String("834cddf4fd794b36ec9d57ba6fe2e7812022aa521f39ddc899ea959680229c79"); //Password
+		  String urls = new String("jdbc:postgresql://ec2-54-155-254-112.eu-west-1.compute.amazonaws.com:5432/d747lpck3drmh3?sslmode=require");
+	      String username = new String("ecwnrxyfpgybjl");
+	      String password = new String("834cddf4fd794b36ec9d57ba6fe2e7812022aa521f39ddc899ea959680229c79");
 	      Connection conn = null;
-	      //FileOutputStream fileOutputStream = null;  
-	      String mytableOrQuery="(select ordernumber,bsnumber,datestart,dateend,calc,calcnds,comm from orderform order by ordernumber)";
-	      //String home = System.getProperty("user.home");
-	      //String myfilePath=home+"/Downloads/"+"orders.csv";
-	      //String myfilePath="C:/My/orders.csv";
+	      String myQuery="(select ordernumber,bsnumber,datestart,dateend,calc,calcnds,comm from orderform order by ordernumber)";
 	      InputStreamResource file;
+	      
 	      try {  
 	    	  conn = DriverManager.getConnection(urls, username, password);
-	          CopyManager copyManager = new CopyManager((BaseConnection) conn);  
-	          //fileOutputStream = new FileOutputStream(myfilePath);
+	          CopyManager copyManager = new CopyManager((BaseConnection) conn);
 	          ByteArrayOutputStream out = new ByteArrayOutputStream();
-	          copyManager.copyOut("COPY " + mytableOrQuery + "TO STDOUT WITH (FORMAT CSV, HEADER)", out);
+	          copyManager.copyOut("COPY " + myQuery + "TO STDOUT WITH (FORMAT CSV, HEADER)", out);
 	          ByteArrayInputStream in=new ByteArrayInputStream(out.toByteArray());
 	          file=new InputStreamResource(in);
-	      } finally 
-	      {  
-	          /*if (fileOutputStream != null) {  
-	              try {  
-	                  fileOutputStream.close();  
-	              } catch (IOException e) {  
-	                  e.printStackTrace();  
-	              }  
-	          } */ 
+	      } finally {      
 	          if (conn != null) {
 	              try {
 	                  conn.close();
--- src/main/java/com/example/sbkafka/OrderFormService.java
+++ src/main/java/com/example/sbkafka/OrderFormService.java
@@ -1,8 +1,5 @@
 package com.example.sbkafka;
 
-import java.io.ByteArrayInputStream;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -101,24 +98,5 @@ public class OrderFormService {
 	public double sumDfs() {
 		double sumdfs=orderformrepository.sumDfs();
 		return sumdfs;
-	}
-	
-	public ByteArrayInputStream load() {
-		Sort sort = Sort.by("ordernumber").ascending();
-		Iterable<OrderForm> orders=orderformrepository.findAll(sort);
-		ArrayList<OrderForm> orderlistFile=new ArrayList<OrderForm>();
-            for(OrderForm order:orders) {	
-            int ordernumber=order.getOrderNumber();String bsnumber=order.getBsNumber();
-            String datestart=order.getDateStart();String dateend=order.getDateEnd();
-            double calc=order.getCalc();double calcnds=order.getCalcNds();String comm=order.getComm();
-            OrderForm orderlistarray=new OrderForm(ordernumber,bsnumber,datestart,dateend,calc,calcnds,comm);
-            orderlistFile.addAll(Arrays.asList(orderlistarray));
-         }
-		ByteArrayInputStream in=ExelHelper.orderFormToExcel(orderlistFile);
-		return in;
-	}
-	
-	public void saveToCsv() {
-		orderformrepository.saveToCsv();
 	}
 }
