acbe7bf05bd12cdb4326aa4350d4f56137b8a7ba
Sergey Pavlov <s.a.pavlov@mail.ru>
2024-07-20 10:27:12 +0300

    add new works in price

--- src/main/java/com/example/sbkafka/Controller/PricesController.java
+++ src/main/java/com/example/sbkafka/Controller/PricesController.java
@@ -25,8 +25,8 @@ public class PricesController {
 	@GetMapping("/priceShow") // показать таблицу ТЦП на странице Форма расчетов
 	public String showPricesOrder(Model model) {
 		
-		Price[] price=new Price[47];
-		for (int i=2;i<47;i++) {
+		Price[] price=new Price[49];
+		for (int i=2;i<49;i++) {
 	     
 		price[i]=priceService.getById(i);
 		model.addAttribute("price"+i,price[i]);}
@@ -37,8 +37,8 @@ public class PricesController {
 	@GetMapping("/admin/priceShow") // показать таблицу ТЦП на странице админа
 	public String showPrices(Model model) {
 		
-		Price[] price=new Price[47];
-		for (int i=2;i<47;i++) {
+		Price[] price=new Price[49];
+		for (int i=2;i<49;i++) {
 	     
 		price[i]=priceService.getById(i);
 		model.addAttribute("price"+i,price[i]);}
@@ -52,8 +52,8 @@ public class PricesController {
 		List<String> list = new ArrayList<String>(allParam.values());
 		list.add(0, null);list.add(1, null);
 		String[] paramMassiv=list.toArray(new String[0]);
-		Price[] price=new Price[47];
-		for (int i=2;i<47;i++) { 
+		Price[] price=new Price[49];
+		for (int i=2;i<49;i++) { 
 		price[i]=priceService.getById(i);
 		price[i].setPricesValue(Double.parseDouble(paramMassiv[i]));
 		priceService.savePrice(price[i]);
